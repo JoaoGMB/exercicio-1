3fb130a13acf00607113cf5e4806d0c43720bdfe
JoaoGMB <muller.bordin@gmail.com>
2026-03-23 21:27:31 -0300

    Add files via upload

---
+++ repository/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.seuprojeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.seuprojeto.entity.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
